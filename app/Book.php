4d97e41e3e393e937e491c69705b9782d2408092
Damir Bubanović <damir.bubanovic@yahoo.com>
2020-11-13 11:56:04 +0100

    BookReservations

--- app/Book.php
+++ app/Book.php
@@ -3,6 +3,8 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\User;
+use App\Reservation;
 
 class Book extends Model
 {
@@ -15,4 +17,41 @@ class Book extends Model
             'name'      =>  'author'
         ]))->id;
     }
+
+
+    public function checkout(User $user)
+    {
+    	$this->reservations()->create([
+    		'user_id'			=>	$user->id,
+    		'checked_out_at'	=>	now(),
+    	]);
+    }
+
+
+    public function checkin($user)
+    {
+        $reservation = $this->reservations()
+                    ->where('user_id', $user->id)
+                    ->whereNotNull('checked_out_at')
+                    ->whereNull('checked_in_at')
+                    ->first();
+
+        if (is_null($reservation)) {
+            throw new \Exception();
+        }
+
+
+        $reservation->update([
+            'checked_in_at'     =>  now(),
+        ]);
+    }
+
+
+
+    public function reservations()
+    {
+    	return $this->hasMany(Reservation::class);
+    }
+
+
 }
